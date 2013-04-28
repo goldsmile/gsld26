d4c114b771c2dd0fc9d1a8b4f19c0429921903a4
tangentstorm <michal.wallace@gmail.com>
2013-04-27 21:18:19 -0500

    enable zengl version

--- game.pas
+++ game.pas
@@ -1,6 +1,12 @@
 {$i xpc.inc}
 unit game;
-interface uses xpc, ng, rxgl_sdl;
+interface uses xpc, ng,
+  {$IFDEF SDL}
+    rxgl in 'b4/ng/rxgl_sdl.pas'
+  {$ELSE}
+    rxgl in 'b4/ng/rxgl_zen.pas'
+  {$ENDIF}
+  ;
 
 procedure Main;
 
@@ -10,7 +16,7 @@ procedure Main;
 var vm : TRetroVM;
 begin
   vm := TRetroVM.Create( 'b4/lib/retro/retroImage', false, 262144 ); // 1 mb
-  rxgl_sdl.main( vm );
+  rxgl.main( vm );
 end;
 
 end.
